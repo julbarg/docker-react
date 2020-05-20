13d240d4a649d58447de4ce4f609620c9781602e
Julian Barragan <julbarg@gmail.com>
2020-05-20 15:07:46 -0500

    change app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Hi Julian. How are you. I got App worked!!!!!!</p>
+        <p>Hi Julian Barragan Verano. How are you. I got App worked!!!!!!</p>
         <a
           className='App-link'
           href='https://reactjs.org'
